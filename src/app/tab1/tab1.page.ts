8ef7860c4f56b1b5ddd7d6b81e8323cc30935299
martaalopez <119664444+martaalopez@users.noreply.github.com>
2023-12-14 14:32:15 +0100

    probandoMapa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,10 @@
-
 import { Component } from '@angular/core';
 import { AlertController, IonicModule, ModalController } from '@ionic/angular';
-import { NoteService } from '../services/note.service';
-import { Note } from '../model/note';
 import { CommonModule } from '@angular/common';
 import { EditPage } from '../edit/edit.page';
 import { FormsModule } from '@angular/forms';
-
+import { NoteService } from '../services/note.service';
+import { Note } from '../model/note';
 
 @Component({
   selector: 'app-tab1',
@@ -16,82 +14,79 @@ import { FormsModule } from '@angular/forms';
   imports: [IonicModule,CommonModule,FormsModule]
 })
 export class Tab1Page {
+  notes: Note[] = [];
+
   modalCtrl: ModalController;
 
-constructor( private alertController:AlertController,public noteS:NoteService,private modalController:ModalController) {
-  this.modalCtrl=modalController;
-}
+  constructor( private alertController:AlertController,public noteS:NoteService,private modalController:ModalController) {
+    this.modalCtrl=modalController;
+  }
 
-trackNoteKey(index: number, note: Note): string {
-  return note.key? note.key :index.toString(); // Suponiendo que 'key' es una propiedad única en la nota
-}
-
-ionViewDidEnter(){
-  /*this.misnotas=[];
-  this.noteS.readAll().subscribe(d=>{
-    console.log(d)
-    d.docs.forEach((el:any) => {
-      this.misnotas.push({'key':el.id,...el.data()});
+  ionViewDidEnter() {
+    this.noteS.readAll().subscribe((notes: Note[]) => {
+      this.notes = notes;
     });
-  })*/
-}
-public async editNote(note: Note) {
-  const modal = await this.modalCtrl.create({
-    component: EditPage,
-    componentProps: { data: note }
-  });
-  await modal.present();
+  }
 
-  const { data, role } = await modal.onWillDismiss();
-  if (!role) {
-    if (data) {
-      try {
-        await this.noteS.updateNote(data);
-        // Aquí, vuelve a cargar o actualiza la lista de notas, ya que has editado una nota
-        this.noteS.readAll().subscribe((notes: Note[]) => {
-          // Actualiza tu lista de notas con los datos actualizados
-        });
-        console.log("¡Nota actualizada correctamente!");
-      } catch (error) {
-        console.error("Error al actualizar la nota:", error);
+  chunkArray(array: any[], chunkSize: number) {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+  }
+  async deleteNoteConfirmation(note: Note) {
+    const alert = await this.alertController.create({
+      header: 'Confirmar eliminación',
+      message: '¿Estás seguro de que quieres eliminar esta nota?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelado');
+          }
+        },
+        {
+          text: 'Eliminar',
+          handler: () => {
+            this.deleteNote(note);
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  }
+  deleteNote($event: Note) {
+    if ($event.key) {
+      console.log("Eliminando Nota con clave:", $event.key);
+      this.noteS.deleteNote($event);
+    } else {
+      console.error("La nota no tiene una clave válida");
+    }
+  }
+  public async editNote(note: Note) {
+    const modal = await this.modalCtrl.create({
+      component: EditPage,
+      componentProps: { data: note }
+    });
+    await modal.present();
+  
+    const { data, role } = await modal.onWillDismiss();
+    if (!role) {
+      if (data) {
+        try {
+          await this.noteS.updateNote(data);
+          // Aquí, vuelve a cargar o actualiza la lista de notas, ya que has editado una nota
+          this.noteS.readAll().subscribe((notes: Note[]) => {
+            // Actualiza tu lista de notas con los datos actualizados
+          });
+          console.log("¡Nota actualizada correctamente!");
+        } catch (error) {
+          console.error("Error al actualizar la nota:", error);
+        }
       }
     }
   }
-}
-
-async deleteNoteConfirmation(note: Note) {
-  const alert = await this.alertController.create({
-    header: 'Confirmar eliminación',
-    message: '¿Estás seguro de que quieres eliminar esta nota?',
-    buttons: [
-      {
-        text: 'Cancelar',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancelado');
-        }
-      },
-      {
-        text: 'Eliminar',
-        handler: () => {
-          this.deleteNote(note);
-        }
-      }
-    ]
-  });
-
-  await alert.present();
-}
-deleteNote($event: Note) {
-  if ($event.key) {
-    console.log("Eliminando Nota con clave:", $event.key);
-    this.noteS.deleteNote($event);
-  } else {
-    console.error("La nota no tiene una clave válida");
-  }
-}
-
-
-
-
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import *as L from 'leaflet';
 
 @Component({
   selector: 'app-tab3',
@@ -10,5 +11,19 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab3Page {
+  map!: L.Map;
+
   constructor() {}
+  ngOnInit(){
+  }
+  ionViewDidEnter(){
+    this.map=L.map('mapId').setView([37.57903,-4.6553779],13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(this.map);
+const markPoint=L.marker([37.57903,-4.6553779]);
+this.map.addLayer(markPoint);
+
+  }
+  
 }
